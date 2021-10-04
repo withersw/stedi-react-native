03767c192b3fabf5a819bbc051b9e9a827113df5
Sean Murdock <scmurdock@gmail.com>
2021-10-04 17:00:53 -0600

    Created Tab using function call, imported TouchableOpacity from react-native

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { Card, ListItem, Button, Icon, Tab } from 'react-native-elements';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { Card, ListItem, Button, Icon } from 'react-native-elements';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Timer from "./Timer.js";
 import SettingsScreen from "./SettingsScreen.js"
 
 // import Icons from "./Icons";
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
--- Timer.js
+++ Timer.js
@@ -11,4 +11,4 @@ const Timer = () => {
 
 
 
-export default "Timer";
+export default Timer;
