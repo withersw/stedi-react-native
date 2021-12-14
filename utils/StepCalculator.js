b52b279e93ba27488186a678dacf663f802e8d85
Sean Murdock <scmurdock@gmail.com>
2021-12-14 07:18:02 -0700

    Enhance calculator util

--- utils/StepCalculator.js
+++ utils/StepCalculator.js
@@ -1,6 +1,25 @@
 const getSpikesFromAccelerometer = (recentAccelerationData, threshold = 11) =>{
     const overThresholdSpikes = [];//the acceleration data, often look like the following, can you find the spike(s) over the default threshold
-/*     _______
+/*
+[
+{"value": 9.88,
+"time": 1289182498},
+{"value": 9.89,
+"time": 1289182898},
+{"value": 9.88,
+"time": 1289183498},
+{"value": 10.01,
+"time": 1289184498},
+{"value": 11.03,
+"time": 1289185498},
+{"value": 12.45,
+"time": 1289186498},
+{"value": 11.99,
+"time": 1289187498},
+{"value": 11.88,
+"time": 1289188498}
+]
+     _______
     | 9.88  |
      -------
     | 9.89  |
@@ -22,18 +41,18 @@ const getSpikesFromAccelerometer = (recentAccelerationData, threshold = 11) =>{
     let goingUp=false;//when we stop going up, we have hit a spike
     let previousValue = 0;//this is not a real value
     let previousTime = 0;//this is not a real time
+    let previousHighPointTime = 0;
     recentAccelerationData.foreach((accelerationDatum) => {
         
         if (accelerationDatum.value > previousValue && previousValue!=0){
             goingUp = true;
         } else {
-            if (goingUp=true && previousTime !=0 && (accelerationDatum.time-previousTime > 400) && previousValue > threshold){
+            if (goingUp=true && previousTime !=0 && (previousHighPointTime !=0 && accelerationDatum.time-previousHighPointTime > 400) && previousValue > threshold){
                 overThresholdSpikes.push(accelerationDatum);
+                previousHighPointTime = accelerationDatum.time;
             }
             goingUp = false;
-        }
-
-        
+        }        
 
         previousValue = accelerationDatum.value;
     });
